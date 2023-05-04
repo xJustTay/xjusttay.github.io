de7b6a0d9ca50e2a9dc47a1a20845c8fbc82ce19
xJustTay <128424610+xJustTay@users.noreply.github.com>
2023-05-03 20:56:27 -0400

    site upload

---
+++ __resource.lua
@@ -0,0 +1,30 @@
+files {
+    'index.html',
+    'audio.mp3',
+    'css/style.css',
+    'css/font.css',
+    'img/logo.png',
+    'music/loading.ogg',
+    'font/Orbitron-Black',
+    'font/Orbitron-Black.woff2',
+    'font/Orbitron-Bold',
+    'font/Orbitron-Bold.woff2',
+    'font/Orbitron-ExtraBold',
+    'font/Orbitron-ExtraBold.woff2',
+    'font/Orbitron-Medium',
+    'font/Orbitron-Medium.woff2',
+    'font/Orbitron-Regular',
+    'font/Orbitron-Regular.woff2',
+    'font/Orbitron-SemiBold',
+    'font/Orbitron-SemiBold.woff2',
+    'js/music-controls.js',
+    'js/music-handler.js',
+}
+
+loadscreen_manual_shutdown "yes"
+
+client_script "client.lua"
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
